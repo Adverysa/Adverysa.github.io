3d57ab9986fe5041882245d466060f024dbfce6f
Ollie <okbamforth@gmail.com>
2025-02-05 00:33:58 +0000

    Minor fixes and additions

---
+++ JS/scrollbutton.js
@@ -0,0 +1,26 @@
+//Get the button
+let scrollbutton = document.getElementById("scroll-to-top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        scrollbutton.style.display = "block";
+    } else {
+        scrollbutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollbutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
