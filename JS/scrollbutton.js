9a9d3001d9a214f180935e9194054a4dc32f4cdd
Ollie <okbamforth@gmail.com>
2025-02-06 16:47:26 +0000

    Improved code readability

--- JS/scrollbutton.js
+++ JS/scrollbutton.js
@@ -1,26 +1,21 @@
-//Get the button
-let scrollbutton = document.getElementById("scroll-to-top-button");
+let scrollbutton = document.getElementById("scroll-to-top-button"); // Get the button
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
-};
+window.addEventListener("scroll", scrollFunction); // When the user scrolls down 20px from the top of the document, show the button
 
 function scrollFunction() {
-    if (
+    if ( // If the user scrolls down 20px from the top of the document, show the button
         document.body.scrollTop > 20 ||
         document.documentElement.scrollTop > 20
     ) {
-        scrollbutton.style.display = "block";
+        scrollbutton.style.display = "block"; // Display the button
     } else {
-        scrollbutton.style.display = "none";
+        scrollbutton.style.display = "none"; // Hide the button
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-scrollbutton.addEventListener("click", backToTop);
+scrollbutton.addEventListener("click", backToTop);  // When the user clicks on the button, scroll to the top of the document
 
-function backToTop() {
+function backToTop() { // When the user clicks on the button, scroll to the top of the document
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
